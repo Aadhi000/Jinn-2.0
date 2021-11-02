7aed048e4b2ec89df4b77ac4d74e942063cfff59
Aadhi000 <87893573+Aadhi000@users.noreply.github.com>
2021-11-02 16:40:21 +0530

    Update Inline.py

--- LuciferMoringstar_Robot/Filter/Inline.py
+++ LuciferMoringstar_Robot/Filter/Inline.py
@@ -91,10 +91,8 @@ def get_reply_markup(query):
     buttons = [[
         InlineKeyboardButton('Support Group', url='t.me/Mo_Tech_Group'),
         InlineKeyboardButton('More Botz', url='t.me/MT_Botz')
-        ],[
-        InlineKeyboardButton('🖥️ Deploy Video 🖥️', url=f'{TUTORIAL}')
-        ],[
-        InlineKeyboardButton('🔍 Search again 🔎', switch_inline_query_current_chat=query)
+        ],[  
+        InlineKeyboardButton('💞 Search again 💞', switch_inline_query_current_chat=query)
         ]]
     return InlineKeyboardMarkup(buttons)
 
