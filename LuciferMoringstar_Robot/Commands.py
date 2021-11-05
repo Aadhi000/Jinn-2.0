8d8a4998ddf3197ffcdd44fb8d4feab7b67d8235
Aadhi000 <87893573+Aadhi000@users.noreply.github.com>
2021-11-05 17:11:25 +0530

    Update Commands.py

--- LuciferMoringstar_Robot/Commands.py
+++ LuciferMoringstar_Robot/Commands.py
@@ -320,7 +320,7 @@ async def delete(bot, message):
     """Delete file from database"""
     reply = message.reply_to_message
     if reply and reply.media:
-        msg = await message.reply("Processing...⏳", quote=True)
+        msg = await message.reply("Checking....🗳", quote=True)
     else:
         await message.reply('Reply to file with /delete which you want to delete', quote=True)
         return
