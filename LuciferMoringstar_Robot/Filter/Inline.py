7999d2a493268b26bd5a14ff439e6a25056e622c
Aadhi <87893573+Aadhi000@users.noreply.github.com>
2021-12-21 16:48:57 +0530

    Update Inline.py

--- LuciferMoringstar_Robot/Filter/Inline.py
+++ LuciferMoringstar_Robot/Filter/Inline.py
@@ -89,8 +89,8 @@ async def answer(bot, query):
 
 def get_reply_markup(query):
     buttons = [[
-        InlineKeyboardButton('💠 𝗚𝗿𝗼𝘂𝗽 💠', url='t.me/Movies_World02'),
-        InlineKeyboardButton('💠 𝗖𝗵𝗮𝗻𝗻𝗲𝗹 💠', url='t.me/Minnal_Murali2021HD')
+        InlineKeyboardButton('💠 𝗚𝗿𝗼𝘂𝗽 💠', url='https://t.me/MoviesWorld_Group'),
+        InlineKeyboardButton('💠 𝗖𝗵𝗮𝗻𝗻𝗲𝗹 💠', url='https://t.me/Minnal_Murali2021HD')
         ],[  
         InlineKeyboardButton('♻️ 𝗦𝗲𝗮𝗿𝗰𝗵 𝗔𝗴𝗮𝗶𝗻 ♻️', switch_inline_query_current_chat=query)     
         ]]
